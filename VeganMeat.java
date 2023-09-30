9f3f7c35e7e2479b4b9ec4e5b6227a62eabedf38
joelsamuel <samueljoel155@gmail.com>
2023-09-30 16:01:46 -0400

    edited Vegan meat class

--- VeganMeat.java
+++ VeganMeat.java
@@ -1,6 +1,13 @@
+class VeganMeat extends VeganFoodItem {
 
+    private double ouncesAmount;
 
+    public VeganMeat(int id, String name, String brand, double price, String flavor, double ouncesAmount){
+        super(id, name, brand, price, flavor);
+        this.setCalorieAmount(ouncesAmount);
+    }
 
+    public double getOuncesAmount() {return ouncesAmount; }
 
-public class VeganMeat {
+    public void setCalorieAmount(double ouncesAmount) { this.ouncesAmount = ouncesAmount;}
 }
