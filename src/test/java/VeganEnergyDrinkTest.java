0664fca49d85ea4b0a89eeb6ffe44e915b0f652c
joelsamuel <samueljoel155@gmail.com>
2023-11-28 18:15:52 -0500

    Added customer, VeganChips, vegan energy drink and vegan meat unit testing

---
+++ src/test/java/VeganEnergyDrinkTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VeganEnergyDrinkTest {
+    @Test
+    void setCaffeineAmount() {
+
+    }
+    @Test
+    void setSize() {
+
+    }
+}
